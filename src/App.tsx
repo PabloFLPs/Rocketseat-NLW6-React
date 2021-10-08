7d7adba4ea291740d528ede2452a5bff88c035c6
Pablo Felipe <pablofelipe1999@hotmail.com>
2021-10-08 17:13:16 -0300

    homepage and create room with Google page almost done

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,15 @@
+import { Home } from "./pages/Home";
+import { NewRoom } from "./pages/NewRoom";
+
+import {BrowserRouter, Route} from "react-router-dom"
+
 function App() {
   return (
-    <h1>Hello World</h1>
+    <BrowserRouter>
+      <Route path="/" exact={true} component={Home} />
+      <Route path="/rooms/new" component={NewRoom} />
+    </BrowserRouter>
+
   );
 }
 
