b484ca62189c593dfd25f50e39c43b2300c2b787
Pablo Felipe <pablofelipe1999@hotmail.com>
2021-10-14 19:05:05 -0300

    latest modifications

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import { Home } from "./pages/Home";
 import { NewRoom } from "./pages/NewRoom";
 import { Room } from "./pages/Room";
+import { AdminRoom } from "./pages/AdminRoom";
 
 import {BrowserRouter, Route} from "react-router-dom"
 
 import {AuthContextProvider} from "./contexts/AuthContext"
-
 
 function App() {
 
@@ -16,6 +16,8 @@ function App() {
           <Route path="/" exact={true} component={Home} />
           <Route path="/rooms/new" exact={true} component={NewRoom} />
           <Route path="/rooms/:id" component={Room} />
+
+          <Route path="/admin/rooms/:id" component={AdminRoom} />
         </switch>
       </AuthContextProvider>
     </BrowserRouter>
